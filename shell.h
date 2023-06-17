de466db3871cfd8e0e1fef9dab7e51a5ceb36ac8
DWKanyi <dharma_wanjiku@hotmail.com>
2023-06-17 18:50:01 +0300

    Header file

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_INPUT_LENGTH 1024
+
+#endif
