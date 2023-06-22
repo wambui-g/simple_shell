e02fb72ddcd8a16b46fb0b331186c013cea01ac7
wambui-g <grayciekamau03@gmail.com>
2023-06-22 08:50:59 +0000

    updates on shell

--- shell.h
+++ shell.h
@@ -15,11 +15,11 @@ extern char **environ;
 /*---PROTOTYPES---*/
 /* main.c */
 void shell_interactive(void);
-void shell_no_interactive(void);
+void shell_non_interactive(void);
 
 /* shell_interactive.c */
 char *read_line(void);
-char **split_line(char *line);
+char **pars_line(char *input);
 int execute_args(char **args);
 
 /* execute_args */
