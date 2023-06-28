85ca2113c653fcbaee6352c889f68e95bf15de31
wambui-g <grayciekamau03@gmail.com>
2023-06-28 06:53:36 +0000

    meet betty checks

--- shell_interactive.c
+++ shell_interactive.c
@@ -11,18 +11,18 @@ void shell_interactive(void)
 	char **args;
 	int status = -1;
 	char prompt[] = "$ ";
-	
+
 	while (status == -1)
 	{
 		write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
 		input = read_line();
 		args = parse_line(input);
 		status = new_process(args);
-		
+	
 		/* avoid memory leaks */
 		free(input);
 		free(args);
-		
+	
 		/* exit with status */
 		if (status >= 0)
 			exit(status);
