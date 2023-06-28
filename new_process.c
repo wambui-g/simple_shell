65f50c9a1bd1f1012b88af1be62f308658edc323
wambui-g <grayciekamau03@gmail.com>
2023-06-28 06:56:45 +0000

    meet betty checks

--- new_process.c
+++ new_process.c
@@ -5,7 +5,7 @@
  * @args: array of strings that contains the command and its flags
  *
  * Return: 1 if success, 0 otherwise.
- */ 
+ */
 int new_process(char **args)
 {
 	pid_t pid;
@@ -17,7 +17,7 @@ int new_process(char **args)
 	{
 		if (execvp(args[0], args) == -1)
 		{
-			/*perror("No such file or directory");*/
+			perror("No such file or directory");
 			exit(EXIT_FAILURE);
 		}
 		else
@@ -29,8 +29,7 @@ int new_process(char **args)
 	}
 	else
 	{
-		do
-		{
+		do {
 			waitpid(pid, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
