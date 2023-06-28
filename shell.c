47a409d5416d4ecc922c771a84a0600889108343
wambui-g <grayciekamau03@gmail.com>
2023-06-28 19:57:18 +0000

    update

--- shell.c
+++ shell.c
@@ -7,7 +7,27 @@
  */
 int main(void)
 {
-	shell_interactive();
+	char *input;
+	char **args;
+	int status = -1;
+
+	if (isatty(STDIN_FILENO) == 1)
+		shell_interactive();
+	else
+	{
+		while (status == -1)
+		{
+			input = read_line();
+			args = parse_line(input);
+			status = new_process(args);
+
+			free(input);
+			free(args);
+
+			if (status >= 0)
+				exit(status);
+		}
+	}
 
 	return (0);
 }
