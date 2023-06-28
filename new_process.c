3247fbeb82e5a203ab05f7e40441592a15bb26be
wambui-g <grayciekamau03@gmail.com>
2023-06-28 12:02:51 +0000

    update

--- new_process.c
+++ new_process.c
@@ -1,4 +1,6 @@
 #include "shell.h"
+#include <libgen.h>
+
 /**
  * _strcmp- compare strings
  * @str1: first string
@@ -28,7 +30,8 @@ int new_process(char **args)
 	pid_t pid;
 	int status;
 	char *is_exit = "exit";
-	char *error = ": command not found\n";
+	/*char *error = ": command not found\n";*/
+	/*const char *prog_name = basename(args[0]);*/
 
 	if (args[0] == NULL)
 		return (-1);
@@ -42,8 +45,10 @@ int new_process(char **args)
 	{
 		if (execvp(args[0], args) == -1)
 		{
-			/*write(STDERR_FILENO, args[0], sizeof(args));*/
-			perror(error);
+			/*write(STDERR_FILENO, prog_name, sizeof(prog_name));
+			write(STDERR_FILENO, ": ", 2);*/
+			write(STDERR_FILENO, args[0], sizeof(args));
+			perror(" ");
 			exit(EXIT_FAILURE);
 		}
 		else
